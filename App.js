30469093d09beb2f70dcc1a76f74f12dd68ff8ea
Shailesh Siju <shaileshsiju212@gmail.com>
2021-12-30 20:34:20 +0530

    Changes done

--- App.js
+++ App.js
@@ -1,20 +1,214 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, createRef } from "react";
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Text,
+  ScrollView,
+  Image,
+  Keyboard,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+} from "react-native";
+import AsyncStorage from "@react-native-community/async-storage";
 
-export default function App() {
+const LoginScreen = ({ navigation }) => {
+  const [UserNumber, setUserNumber] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errortext, setErrortext] = useState("");
+
+  const passwordInputRef = createRef();
+
+  const handleSubmitPress = () => {
+    setErrortext("");
+    if (!UserNumber) {
+      alert("Please fill Email");
+      return;
+    }
+    if (!userPassword) {
+      alert("Please fill Password");
+      return;
+    }
+    setLoading(true);
+    let dataToSend = { email: UserNumber, password: userPassword };
+    let formBody = [];
+    for (let key in dataToSend) {
+      let encodedKey = encodeURIComponent(key);
+      let encodedValue = encodeURIComponent(dataToSend[key]);
+      formBody.push(encodedKey + "=" + encodedValue);
+    }
+    formBody = formBody.join("&");
+
+    fetch("http://localhost:3000/api/user/login", {
+      method: "POST",
+      body: formBody,
+      headers: {
+        //Header Defination
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        //Hide Loader
+        setLoading(false);
+        console.log(responseJson);
+        // If server response message same as Data Matched
+        if (responseJson.status === "success") {
+          AsyncStorage.setItem("user_id", responseJson.data.email);
+          console.log(responseJson.data.email);
+          navigation.replace("DrawerNavigationRoutes");
+        } else {
+          setErrortext(responseJson.msg);
+          console.log("Please check your email id or password");
+        }
+      })
+      .catch((error) => {
+        //Hide Loader
+        setLoading(false);
+        console.error(error);
+      });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.mainBody}>
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{
+          flex: 1,
+          justifyContent: "center",
+          alignContent: "center",
+        }}
+      >
+        <View>
+          <KeyboardAvoidingView enabled>
+            <View style={{ alignItems: "center", margin: 20 }}>
+              <Image
+                source={require("./assets/cricket-player.png")}
+                style={{
+                  width: "100%",
+                  height: 180,
+                  resizeMode: "contain",
+                }}
+              />
+              <Text
+                style={{
+                  fontSize: 35,
+                  fontWeight: "bold",
+                  fontStyle: "italic",
+                }}
+              >
+                Cricket Auction
+              </Text>
+            </View>
+            <View style={styles.SectionStyle}>
+              <TextInput
+                style={styles.inputStyle}
+                onChangeText={(UserNumber) => setUserNumber(UserNumber)}
+                placeholder="Enter Number" //dummy@abc.com
+                placeholderTextColor="#8b9cb5"
+                autoCapitalize="none"
+                keyboardType="name-phone-pad"
+                returnKeyType="next"
+                onSubmitEditing={() =>
+                  passwordInputRef.current && passwordInputRef.current.focus()
+                }
+                underlineColorAndroid="#f000"
+                blurOnSubmit={false}
+              />
+            </View>
+            <View style={styles.SectionStyle}>
+              <TextInput
+                style={styles.inputStyle}
+                onChangeText={(UserPassword) => setUserPassword(UserPassword)}
+                placeholder="Enter Password" //12345
+                placeholderTextColor="#8b9cb5"
+                keyboardType="default"
+                ref={passwordInputRef}
+                onSubmitEditing={Keyboard.dismiss}
+                blurOnSubmit={false}
+                secureTextEntry={true}
+                underlineColorAndroid="#f000"
+                returnKeyType="next"
+              />
+            </View>
+            {errortext != "" ? (
+              <Text style={styles.errorTextStyle}>{errortext}</Text>
+            ) : null}
+            <TouchableOpacity
+              style={styles.buttonStyle}
+              activeOpacity={0.5}
+              onPress={handleSubmitPress}
+            >
+              <Text style={styles.buttonTextStyle}>LOGIN</Text>
+            </TouchableOpacity>
+            <Text
+              style={styles.registerTextStyle}
+              onPress={() => navigation.navigate("RegisterScreen")}
+            >
+              New Here ? Register
+            </Text>
+          </KeyboardAvoidingView>
+        </View>
+      </ScrollView>
     </View>
   );
-}
+};
+export default LoginScreen;
 
 const styles = StyleSheet.create({
-  container: {
+  mainBody: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    backgroundColor: "#307ecc",
+    alignContent: "center",
+  },
+  SectionStyle: {
+    flexDirection: "row",
+    height: 40,
+    marginTop: 20,
+    marginLeft: 35,
+    marginRight: 35,
+    margin: 10,
+  },
+  buttonStyle: {
+    backgroundColor: "#7DE24E",
+    borderWidth: 0,
+    color: "#FFFFFF",
+    borderColor: "#7DE24E",
+    height: 40,
+    alignItems: "center",
+    borderRadius: 30,
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 20,
+    marginBottom: 25,
+  },
+  buttonTextStyle: {
+    color: "#FFFFFF",
+    paddingVertical: 10,
+    fontSize: 16,
+  },
+  inputStyle: {
+    flex: 1,
+    color: "white",
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderWidth: 1,
+    borderRadius: 30,
+    borderColor: "#dadae8",
+  },
+  registerTextStyle: {
+    color: "#FFFFFF",
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 14,
+    alignSelf: "center",
+    padding: 10,
+  },
+  errorTextStyle: {
+    color: "red",
+    textAlign: "center",
+    fontSize: 14,
   },
 });
